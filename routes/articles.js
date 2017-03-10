51855a52bf029f9656fc23123c9fd5248b38e58d
lvlay <lvlayster@gmail.com>
2017-03-10 11:34:00 +0800

    实现搜索和分页

--- routes/articles.js
+++ routes/articles.js
@@ -99,5 +99,62 @@ router.get("/delete/:_id",function (req,res,next) {
     })
 })
 
+//get   /articles/list/3/2
+//post  /articles/list/1/2
+router.all('/list/:pageNum/:pageSize',function (req,res,next) {
+    //pageNum表示当前是第几页 默认值是第一页
+    var searchBtn = req.body.searchBtn;
+    var pageNum = parseInt(req.params.pageNum) && parseInt(req.params.pageNum)>0? parseInt(req.params.pageNum) : 1;
+    // pageSize 表示每一页有多少记录,默认2条
+    var pageSize = parseInt(req.params.pageSize) && parseInt(req.params.pageSize)>0? parseInt(req.params.pageSize) : 2;
+
+
+    //搜索条件
+    var query={};
+     //这种情况是只有是点了搜索按钮时,才能拿到keyword
+    var keyword =  req.body.keyword;
+    if(searchBtn)
+    {
+        //是点击了searchBtn按钮的
+        //把关键字存到session中防止丢失
+        req.session.keyword =keyword;
+    }
+
+    if(req.session.keyword)
+    {
+        query['title'] = new RegExp(req.session.keyword,'ig');
+    }
+
+    // 首先要知道这个搜索结果一共有多少条记录,方便计算页数
+    Model('Article').count(query,function (err,count) {
+
+        console.log('count='+count+"  pageNum="+pageNum+" pageSie="+pageSize+" skip="+(pageNum-1)*pageSize);
+        //查询符合条件的当前这一页的数据
+        Model('Article').find(query)
+            .sort({'createTime':-1})//按时间排倒叙
+            .skip((pageNum-1)*pageSize)
+            .limit(pageSize)
+            .populate('user')
+            .exec(function(err,articles){
+                console.log(articles);
+                res.render('index',{
+                    title:'首页',
+                    articles:articles,
+                    count:count,
+                    pageNum:pageNum,
+                    keyword:req.session.keyword?req.session.keyword:'',
+                    pageSize:pageSize
+                })
+
+            })
+    })
+        
+
+})
+
+
+
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,23 @@ var md = require('markdown').markdown;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   //查询数据库 获取文章列表
-  Model('Article').find({}).populate('user')
-      .exec(function (err,articles) {
+  //  //查询数据库 获取文章列表
+  // Model('Article').find({}).populate('user')
+  //     .exec(function (err,articles) {
+  //
+  //         articles.forEach(function (a) {
+  //
+  //             //将markdown语法的文章内容转换成html格式
+  //             a.content =  md.toHTML(a.content);
+  //         })
+  //
+  //       res.render('index', { title: '我的博客',articles:articles });
+  //     });
 
-          articles.forEach(function (a) {
+    //切换到首页时,要清空session中的搜索关键字
+    req.session.keyword=null;
+    res.redirect("/articles/list/1/2");
 
-              //将markdown语法的文章内容转换成html格式
-              a.content =  md.toHTML(a.content);
-          })
-
-        res.render('index', { title: '我的博客',articles:articles });
-      });
 });
 
 module.exports = router;
